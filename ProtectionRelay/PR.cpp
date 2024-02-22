ce495f0afb0b7934f79d26aa193113d6aed5f08c
ytaekk <151015780+ytaekk@users.noreply.github.com>
2024-02-22 23:32:56 +0900

    (Update) Include File Writing and OCR
    1. Include File Writing
     - For writing relay operating record. It's is TXT file, but in this txt file I made a tap between each element. So If you want to using Excel, then you just Copy n Paste on Excel. The it will automatically input element on each cell.
    2. Include OCR
     - It is Over Current Relay. If Current(now) is higher then setting current then OCR will be operate. It's basic Relay.

--- ProtectionRelay/PR.cpp
+++ ProtectionRelay/PR.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>	// for print file
+#include <chrono>	// for time
+#include <iomanip>	// for time sort
+#include <sstream>	// for time function (nowTime) on File
 
 using namespace std;
 
@@ -6,9 +10,9 @@ class RelayElement
 {
 	bool _onoff;
 	bool _setTrip;
-	int _setVolt;
-	int _setTime;
-	int _setCurrent;
+	double _setVolt;
+	double _setTime;
+	double _setCurrent;
 
 
 public:
@@ -24,10 +28,16 @@ public:
 	{
 		_onoff = in_onoff;
 	}
-	void setRelay(bool in_trip,int in_volt, int in_time)
+	void setVoltRelay(bool in_trip, double in_volt, double in_time)
 	{
 		_setTrip = in_trip;
 		_setVolt = in_volt;
+		_setTime = in_time;
+	}
+	void setCurRelay(bool in_trip, double in_current, double in_time)
+	{
+		_setTrip = in_trip;
+		_setCurrent = in_current;
 		_setTime = in_time;
 	}
 	bool getTrip()
@@ -42,15 +52,20 @@ public:
 	{
 		return _setVolt;
 	}
+	int getCurrent()
+	{
+		return _setCurrent;
+	}
 	int getTime()
 	{
 		return _setTime;
 	}
 	void printStatus() // Print Relay Status
 	{
+		cout << "------------------" << endl;
 		cout << "1. Status : " << (_onoff ? "ON" : "OFF") << endl;
 		cout << "2. Alarm Type : " << (_setTrip ? "Trip" : "Alarm") << endl;
-		cout << "3. Volatge Set Value : " << _setVolt << " V" << endl;
+		cout << "3. Set Value : " << _setVolt << " V\t" <<_setCurrent<< " A" << endl;
 		cout << "4. Time Set Value : " << _setTime << " Sec" << endl;
 	}
 	void printMenu()
@@ -59,7 +74,9 @@ public:
 		cout << "-  Relay SETTING  -" << endl;
 		cout << "-  1. UVR ( 27 )  -" << endl;
 		cout << "-  2. OVR ( 59 )  -" << endl;
-		cout << "-  3. Test Relay  -" << endl;
+		cout << "-  3. OCR (50/51) -" << endl;
+		cout << "-  4. Test Relay  -" << endl;
+		cout << "-  5. Quit        -" << endl;
 		cout << "-------------------" << endl;
 		cout << "Insert Number : ";
 	}
@@ -74,13 +91,25 @@ public:
 };
 class RelayType
 {
+	
 public:
 	RelayElement uvr;
 	RelayElement ovr;
+	RelayElement ocr;
+
 	RelayType(){}
 	bool _runOVR = 0;
 	bool _runUVR = 0;
+	bool _runOCR = 0;
 
+
+	void testRelay()
+	{
+		cout << "------------------" << endl;
+		cout << "    Test Relay    " << endl;
+		cout << "0. No       1. Yes" << endl;
+		cout << "Select Number : ";
+	}
 	void operateRelay()
 	{
 		if (_runOVR && ovr.getTrip())
@@ -91,32 +120,95 @@ public:
 			cout << "[** ALARM **] \n UVR(27) is Operated!!\n[** TRIP **] \n CB is Opened" << endl;
 		else if (_runUVR && !uvr.getTrip())
 			cout << "[** ALARM **] \n UVR(27) is Operated!!" << endl;
+		else if (_runOCR && ocr.getTrip())
+			cout << "[** ALARM **] \n OCR(50/51) is Operated!!\n[** TRIP **] \n CB is Opened" << endl;
+		else if (_runOCR && !ocr.getTrip())
+			cout << "[** ALARM **] \n OCR(50/51) is Operated!!" << endl;
 		_runOVR = 0;
 		_runUVR = 0;
+		_runOCR = 0;
 	}
 
-	void compareVR(float test_volt, float test_time)
+	
+	string nowTime() // For Write Time on File
+	{
+		// 현재 시간을 system_clock으로부터 얻기
+		auto now = std::chrono::system_clock::now();
+
+		// 시간을 time_t 타입으로 변환
+		auto now_c = std::chrono::system_clock::to_time_t(now);
+
+		// tm 구조체로 변환하여 로컬 시간으로 조정
+		std::tm now_tm;
+
+		errno_t err = localtime_s(&now_tm, &now_c);
+		stringstream time;
+		time << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S");
+		return time.str();
+	}
+	void saveFile(double test_volt, double test_current, double test_time) // Relay Operated Record
+	{
+		string name;
+
+		if (_runOVR)
+			name = "OVR";
+		else if (_runUVR)
+			name = "UVR";
+		else if (_runOCR)
+			name = "OCR";
+
+		ofstream file("Relay Operating Record.txt",ios::app);
+
+		if (file.is_open()) {
+			if (name == "OCR")
+			{
+				file << nowTime() << "\t" << name << " Operated\tOperated Value : "
+					<< test_current << "A\tOperated Time : "
+					<< test_time << "s" << endl;
+			}
+			else
+			{
+				file << nowTime() << "\t" << name << " Operated\tOperated Value : "
+					<< test_volt << "V\tOperated Time : "
+					<< test_time << "s" << endl;
+			}
+			file.close();
+		}
+		else {
+			cerr << "Cannot Open File" << endl;
+		}
+
+	}
+	void compareVR(double test_volt, double test_current, double test_time)
 	{
 
 		if (ovr.getOnoff() && (test_volt >= ovr.getVolt()))
 			if (test_time >= ovr.getTime()) {
 				_runOVR = 1;
+				saveFile(test_volt,test_current, test_time);
 				operateRelay();
 			}
 		if (uvr.getOnoff() && (uvr.getVolt() >= test_volt))
 			if (test_time >= uvr.getTime()) {
 				_runUVR = 1;
+				saveFile(test_volt, test_current, test_time);
+				operateRelay();
+			}
+		if (ocr.getOnoff() && (test_current >= ocr.getCurrent()))
+			if (test_time >= ocr.getTime()) {
+				_runOCR = 1;
+				saveFile(test_volt, test_current, test_time);
 				operateRelay();
 			}
 	}
-	
+
 };
 int main() 
 {
 	int menu,edit;
 	bool in_onoff, in_trip;
-	int in_volt, in_time;
-	double test_volt, test_time;
+	double in_volt, in_time, in_current;
+	double test_volt, test_time, test_current;
 
 	RelayType relay;
 
@@ -126,6 +218,9 @@ int main()
 		relay.uvr.printMenu();
 		cin >> menu;
 		cout << endl;
+		
+		if (menu == 5)	//Quit Program
+			break;
 
 		switch (menu)
 		{
@@ -142,7 +237,7 @@ int main()
 			{
 				//UVR on/off 
 				cout << "UVR (27) Relay" << endl;
-				cout << "0.off     1.on" << endl;
+				cout << "0.OFF     1.ON" << endl;
 				cout << "Select Number : ";
 				cin >> in_onoff;
 				relay.uvr.onRelay(in_onoff);
@@ -164,7 +259,7 @@ int main()
 					cout << "Insert Time : ";
 					cin >> in_time;
 					cout << endl;
-					relay.uvr.setRelay(in_trip, in_volt, in_time);
+					relay.uvr.setVoltRelay(in_trip, in_volt, in_time);
 					cout << "UVR(27) Setting Compelete!" << endl;
 					break;
 				}
@@ -184,7 +279,7 @@ int main()
 			if (edit)
 			{
 				cout << "OVR (59) Relay" << endl;
-				cout << "0.off     1.on" << endl;
+				cout << "0.OFF     1.ON" << endl;
 				cout << "Select Number : ";
 				cin >> in_onoff;
 				relay.ovr.onRelay(in_onoff);
@@ -206,7 +301,7 @@ int main()
 					cout << "Insert Time : ";
 					cin >> in_time;
 					cout << endl;
-					relay.ovr.setRelay(in_trip, in_volt, in_time);
+					relay.ovr.setVoltRelay(in_trip, in_volt, in_time);
 					cout << "OVR(59) Setting Compelete!" << endl;
 					break;
 				}
@@ -215,18 +310,70 @@ int main()
 			}
 			else
 				break;
-		case 3: //Test Mode
-			//Input Test Volt and Time
-			cout << "Test Relay" << endl;
-			cout << "Insert Volt : ";
-			cin >> test_volt;
-			cout << "Insert Time : ";
-			cin >> test_time;
-			
-			// Run Relay
-			relay.compareVR(test_volt, test_time);
+			//OCR
+		case 3: 
+			relay.ocr.printStatus();
+			cout << endl;
+			relay.ocr.editSetting();
+			cin >> edit;
+			cout << endl;
 
+			if (edit)
+			{
+				cout << "OCR (50/51) Relay" << endl;
+				cout << "0.OFF     1.ON" << endl;
+				cout << "Select Number : ";
+				cin >> in_onoff;
+				relay.ocr.onRelay(in_onoff);
+				cout << endl;
+
+				//Setting OVR Alarm,Volt,Time
+				if (in_onoff)
+				{
+					cout << "OCR(50/51) Select Alarm Type" << endl;
+					cout << "0.Alarm    1.Trip" << endl;
+					cout << "Select Number : ";
+					cin >> in_trip;
+					cout << endl;
+					cout << "OCR(50/51) Setting Current(CT Ratio 400/5)" << endl;
+					cout << "Insert Current : ";
+					cin >> in_current;
+					cout << endl;
+					cout << "Insert OCR(50/51) Relay Setting Time(sec)" << endl;
+					cout << "Insert Time : ";
+					cin >> in_time;
+					cout << endl;
+					relay.ocr.setCurRelay(in_trip, in_current, in_time);
+					cout << "OCR(50/51) Setting Compelete!" << endl;
+					break;
+				}
+				else
+					break;
+			}
+			else
+				break;
+		case 4: //Test Mode
+			//Input Test Volt and Time
+			relay.testRelay();
+			cin >> edit;
+			cout << endl;
+			if (edit)
+			{
+				cout << "Test Relay" << endl;
+				cout << "Insert Volt : ";
+				cin >> test_volt;
+				cout << "Insert Current : ";
+				cin >> test_current;
+				cout << "Insert Time : ";
+				cin >> test_time;
+
+				// Run Relay and Write File
+				relay.compareVR(test_volt, test_current, test_time);
+			}
+		case 5:	//Quit
+			break;
 		}
+		
 	}
 	return 0;
 }
